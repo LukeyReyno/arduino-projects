192076bdd897d9184075ad82d0ac9844c39f7b0f
Lucas Reyna <lucas.reyna143@gmail.com>
2021-07-17 17:45:05 -0700

    added some button functionality to counter

--- Seven_Segment/sevenSegCounter.ino
+++ Seven_Segment/sevenSegCounter.ino
@@ -7,6 +7,7 @@
 #define ANODE_2 12
 #define ANODE_3 11
 #define ANODE_4 10
+#define BUTTON 9
 #define NUM_ANODES 4
 
 Byte anodes[] = {ANODE_1, ANODE_2, ANODE_3, ANODE_4};
@@ -15,14 +16,27 @@ SevenSegmentDisplay sseg(anodes, NUM_ANODES);
 
 void setup()
 {
-    Serial.begin(115200);
+    pinMode(BUTTON, INPUT);
     sseg.begin();
 }
 
 void loop()
 {
+    if (digitalRead(BUTTON))
+    {
+        delay(1000);
+        count(9999);
+    }
+}
+
+void count(int numSeconds)
+{
     int i;
 
-    for (i = 0; i < 3000; i++)
+    for (i = 0; i < numSeconds; i++)
+    {
         sseg.displayNum(i, 1);
+        if (digitalRead(BUTTON))
+            break;
+    }
 }
